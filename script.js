aa23d2427d1932bd347a10df630be1d08cfffa98
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-10-15 11:51:58 -0700

    Added an ID to the form tag, and started a validation function in the script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Validating the fields
+document.getElementById("account-validation").onsubmit.value = function() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    let confirmPass = document.getElementById("confirm-password").value;
+
+
+
+}
