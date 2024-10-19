918e116293b3fd506737bf89c25465ada70b97ac
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-10-18 17:23:12 -0700

    Did some changes in the index.html file. Finished the styling in the style.css file. Added some started code in JavaScript.

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 // Validating the fields
 document.getElementById("account-validation").onsubmit.value = function() {
 
+    // Validation
+    let isValid = true;
+
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let pass = document.getElementById("password").value;
     let confirmPass = document.getElementById("confirm-password").value;
 
+    if (name == "") {
+        document.getElementById("err-name").style.display = "block";
+        isValid = false;
+    }
 
+    if (email == "") {
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
 
+    if (pass == "") {
+        document.getElementById("err-password").style.display = "block";
+        isValid = false;
+    }
+
+    if (confirmPass == "") {
+        document.getElementById("err-confirm-pass").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for (let i = 0; errors.length; i++) {
+        errors[i].style.display = "none";
+    }
 }
